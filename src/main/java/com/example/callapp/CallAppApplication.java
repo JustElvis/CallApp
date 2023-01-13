77b76c0d4f405b29b8df4c4ccf4c4737fc1a61a5
Yaroslav Shkvarla <slav.shkvarlo@gmail.com>
2023-01-13 16:18:21 +0200

    second commit

---
+++ src/main/java/com/example/callapp/CallAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.callapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallAppApplication.class, args);
+    }
+
+}
